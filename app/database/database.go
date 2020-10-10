6ffdc454bf0db4f52bfcf7749501e7cb30f93ec9
shin04 <daikon0413@gmail.com>
2020-10-10 22:55:24 +0900

    first commit

---
+++ app/database/database.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+var err error
+
+// Init initializes database
+func Init() {
+	err := godotenv.Load(fmt.Sprintf("../%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	DB_USER := os.Getenv("DB_USER")
+	DB_PASS := os.Getenv("DB_PASS")
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_NAME := os.Getenv("DB_NAME")
+
+	dsn := DB_USER + ":" + DB_PASS + "@tcp(" + DB_HOST + ":3306)/" + DB_NAME
+	dsn += "?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		println("db connection success !!")
+	}
+}
+
+// GetDB is called in models
+func GetDB() *gorm.DB {
+	return db
+}
